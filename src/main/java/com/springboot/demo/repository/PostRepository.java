35807c00ffbc61f468cae7f056f652a75fba27f8
Yukti Garg <yukgarg@publicisgroupe.net>
2022-04-28 18:17:04 +0530

    Commit

--- src/main/java/com/springboot/demo/repository/PostRepository.java
+++ src/main/java/com/springboot/demo/repository/PostRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.springboot.demo.entity.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-
+   Post findByName(String name);
 }
