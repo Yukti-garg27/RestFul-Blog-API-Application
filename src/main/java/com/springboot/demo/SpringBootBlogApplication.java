0e4554767cebb6e1743cd945fa18873439d67d6f
Yukti Garg <gargyukti27@gmail.com>
2022-05-18 01:40:29 +0530

    Insert metadata in SQL

--- src/main/java/com/springboot/demo/SpringBootBlogApplication.java
+++ src/main/java/com/springboot/demo/SpringBootBlogApplication.java
@@ -1,12 +1,17 @@
 package com.springboot.demo;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.springboot.demo.entity.Role;
+import com.springboot.demo.repository.RoleRepository;
+
 @SpringBootApplication
-public class SpringBootBlogApplication {
+public class SpringBootBlogApplication implements CommandLineRunner{
 	@Bean
 	public ModelMapper mapper() {
 		return new ModelMapper();
@@ -18,5 +23,19 @@ public class SpringBootBlogApplication {
 		
 		
 	}
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Role adminRole=new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+		
+		Role userRole=new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+	}
+	
 
 }
